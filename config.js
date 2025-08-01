164aea0a1004d0e16f837f11492d22bc1284ff38
hirunvikasitha375-afk <hirunvikasitha375@gmail.com>
2025-08-01 15:15:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "lURFyABC#QHs3FFAl1N3jcZxqT4iEmftOR_1wB4pOll2CZx2SkGo",
+    SESSION_ID: process.env.SESSION_ID || "𝗛𝗜𝗥𝗨-𝗫-𝗠𝗗=JdxlyQ5S#I4tatDpQ1eXTa6ycyBMtl-4XWZOxJmv_cCpYXWsV1gs",
     // add your Session Id 
 
     AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
